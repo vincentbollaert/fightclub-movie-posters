d6837179dc52fb541aba01d98e98f903eb151ed2
Vincent Bollaert <vincentbollaert@gmail.com>
2025-04-13 17:38:12 +0200

    feat: slice images into columns and add basic image perf

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://image.tmdb.org/t/p/original/*')],
+    minimumCacheTTL: 2678400, // 31 days
   },
 }
 
