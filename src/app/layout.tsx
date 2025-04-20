d4f14fa28bea387bb0fc027227d9e46154c7c276
Vincent Bollaert <vincentbollaert@gmail.com>
2025-04-20 18:47:21 +0200

    feat: add custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Lacquer } from 'next/font/google'
 import './globals.css'
 import { ApolloWrapper } from './_components/apolloProvider'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const lacquerFont = Lacquer({
+  variable: '--font-lacquer',
   subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: '400'
 })
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${lacquerFont.variable} antialiased`}>
         <ApolloWrapper>
           <div className="container mx-auto py-8">
             <div className="outer">{children}</div>
