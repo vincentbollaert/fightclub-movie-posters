e16168f3da1ab8b2be5cb9193388bd8d6d7b2dd2
Vincent Bollaert <vincentbollaert@gmail.com>
2025-04-22 19:27:43 +0200

    refactor: move senty values to env

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,11 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://84c1dcf33059408bdd6ad1cc45f91c00@o4509197991346176.ingest.de.sentry.io/4509197993705552",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? '1'),
+  debug: process.env.NODE_ENV !== 'production',
+})
